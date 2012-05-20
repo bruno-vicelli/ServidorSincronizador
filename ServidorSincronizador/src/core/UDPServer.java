2ed6df6359168178b3445248bc1814ae02f60be6
Thiago Cassio Krug <thiagockrug@gmail.com>
2012-05-20 17:40:15 -0300

    udpserver

--- ServidorSincronizador/src/core/UDPServer.java
+++ ServidorSincronizador/src/core/UDPServer.java
@@ -17,14 +17,15 @@ import java.util.logging.Logger;
  * @author thiago
  */
 public class UDPServer {
-public static void main(String[] args) {
+
+    public static void main(String[] args) {
         // socket
         DatagramSocket serverSocket;
         try {
             // instancia ele
             serverSocket = new DatagramSocket(9876);
-            byte[] receiveData = new byte[0];
-            byte[] sendData = new byte[0];
+            byte[] receiveData = new byte[1024];
+            byte[] sendData = new byte[1024];
             while (true) {
                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                 serverSocket.receive(receivePacket);
@@ -32,8 +33,7 @@ public static void main(String[] args) {
                 System.out.println("RECEIVED: " + sentence);
                 InetAddress IPAddress = receivePacket.getAddress();
                 int port = receivePacket.getPort();
-                String capitalizedSentence = sentence.toUpperCase();
-                sendData = capitalizedSentence.getBytes();
+                sendData = String.valueOf(System.currentTimeMillis()).getBytes();
                 DatagramPacket sendPacket =
                         new DatagramPacket(sendData, sendData.length, IPAddress, port);
                 serverSocket.send(sendPacket);
